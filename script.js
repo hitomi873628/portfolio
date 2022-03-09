f72a46a92a86f7e24384e061ec9f508cc03d7bb5
hitomi873628 <101234564+hitomi873628@users.noreply.github.com>
2022-03-09 13:46:13 +0900

    Create script.js

---
+++ script.js
@@ -0,0 +1,64 @@
+$(function(){
+    $('#login-show').click(function(){
+      $('#login-modal').fadeIn();
+
+    });
+
+    $('.close-modal').click(function(){
+      $('#login-modal').fadeOut();
+    });
+    
+
+
+
+    $('#main').hover(function(){
+        $('#welcome-message').fadeIn(3000);
+    });
+
+    $('#welcome-message').fadeIn(function(){
+        $('#aboutme-message').fadeIn(1000);
+        $('#aboutme-message').slideUp(1000);
+        $('#aboutme-message').fadeIn(1000);
+     });
+ 
+     $('#contact-').hover(function(){
+        $('#welcome-message').fadeIn(3000);
+    });
+
+
+
+
+
+
+    $('.skill-hover').hover (
+      function(){
+        $(this).find('.text-contents').addClass('text-active');
+      },
+      function(){
+        $(this).find('.text-contents').removeClass('text-active');
+      }
+      );
+  
+
+
+
+
+
+
+
+    $('.faq-list-item').click(function(){
+        var $answer=$(this).find('.answer');
+        if ($answer.hasClass('open')){
+          $answer.removeClass('open');
+          $answer.slideUp();
+          $(this).find('span').text('+');
+        }else{
+          $answer.addClass('open');
+          $answer.slideDown();
+          $(this).find('span').text('-');
+        }
+      });
+    
+
+
+});
